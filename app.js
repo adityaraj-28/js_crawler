4c8aeaf6ffa4d7889f80c55fb8675ba7a3dc11b4
Aditya Raj <aditya.raj@synaptic.com>
2023-04-03 19:37:11 +0530

    add runner in todo

--- app.js
+++ app.js
@@ -1,17 +1,56 @@
-const { main } = require('./website_crawler.js');
-// const db = require("./db");
+const { website_crawler } = require('./website_crawler.js');
+const db = require("./db");
+const { LEVEL_LIMIT } = require("./constants");
 
-const event = { body: {raw_url: 'apple.com'} };
-const context = {level: 0};
+function fetch_unprocessed_urls(level) {
+    const results = []
+    const query = `select domain, url from crawl_status_2 where level=${level} and status=false`
+    db.query(query, (err, res) => {
+        if(err) {
+            console.log(`error getting urls at level: ${level}`)
+        }
+        res.forEach(row => {
+            results.push(row.url)
+        })
+    })
+    return results
+}
 
-const callback = (error, response) => {
-    if (error) {
-        console.log("Error here")
-        console.error(error);
-    } else {
-        console.log("Success Here")
-        console.log(response);
-    }
-};
-
-main(event, context, callback);
+function get_root_domain(){
+    const domains = []
+    db.query(`select name from domains`, (err, res) => {
+        if(err){
+            console.log('error fetching root domain')
+            process.exit(0)
+        }
+        res.forEach(row => {
+            domains.push(row.name)
+        })
+    });
+    return domains;
+}
+// website_crawler(event, context, callback);
+function run() {
+    const callback = (error, response) => {
+        if (error) {
+            console.log("Error here")
+            console.error(error);
+        } else {
+            console.log("Success Here")
+            console.log(response);
+        }
+    };
+    const root_domains = get_root_domain()
+    root_domains.forEach(domain => {
+        const event = {body: {raw_url: domain}};
+        const context = {level: 0}
+        website_crawler(event, context, (err, res) => {
+            if(err){
+                console.log(err)
+            } else {
+                // we need url, domain and level info here from the callback, so we need to change
+                // what is written on db and what is send to callback response
+            }
+        })
+    })
+}
--- constants.js
+++ constants.js
@@ -65,5 +65,5 @@ module.exports = {
     MAX_URLS_ALLOWED: 20,
     PROTOCOLS: ['https://', 'http://', 'www.'],
     USER_AGENT: 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.0 Safari/537.36',
-    LEVEL_LIMIT: 2
+    LEVEL_LIMIT: 1
 }
